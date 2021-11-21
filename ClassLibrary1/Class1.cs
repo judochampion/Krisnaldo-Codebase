adca37aa58e72aa3883a3d3d30fa7ed852a181fc
Joachim Ally <joachimally@gmail.com>
2021-11-21 23:54:07 +0100

    Kleine Try 2

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -4,6 +4,6 @@ namespace ClassLibrary1
 {
     public static class Class1
     {
-        public static string Return_Message() => $"Hello World, KDB! {DateTime.Now.ToString()}";
+        public static string Return_Message() => $"Hello World, KDB, Try 2! {DateTime.Now.ToString()}";
     }
 }
